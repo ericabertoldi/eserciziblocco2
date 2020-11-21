41a5284846dc8159cf5c706734b3d1209041b314
ericabertoldi <erica.bertoldi01@gmail.com>
2020-11-21 12:20:30 +0100

    es 2.12

---
+++ esercizio_2.12/esercizio_2.12.cpp
@@ -0,0 +1,33 @@
+/* SUB che sceglie un numero a random tra 1 e 1000. Usare la funzione rand per fare questo. L'utente poi
+inserisce numeri ntanto che non azzecca la risposta corretta. Il programma guida l'utente rispondendo
+se il tentativo era maggiore o minore del numero estratto. È una ricerca di tipo binario. */
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+ srand(time(NULL)); //inizializza i valori
+  //cout<<rand()%1000+1<<endl; -> COSI' STAMPEREBBE UN NUMERO COMPRESO TRA 1 E 1000
+ int rd;
+ rd= rand()%1000+1;
+ int n;
+ cout<<"guess random number"<<endl;
+ cin>>n;
+ 
+ if (n==rd) {
+     cout<<"correct!"<<endl;
+ }
+ else while (n<rd) {
+     cout<<"try with a larger number"<<endl;
+     cin>>n;
+ }
+ while (n>rd) {
+     cout<<"try with a smaller number"<<endl;
+     cin>>n;
+ }
+ 
+ return 0;
+}
