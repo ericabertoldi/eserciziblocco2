bf132af1c927b60494f1dc2531808236085ad8f9
ericabertoldi <erica.bertoldi01@gmail.com>
2020-11-21 13:00:30 +0100

    es 2.12

--- esercizio_2.12/esercizio_2.12.cpp
+++ esercizio_2.12/esercizio_2.12.cpp
@@ -2,32 +2,32 @@
 inserisce numeri ntanto che non azzecca la risposta corretta. Il programma guida l'utente rispondendo
 se il tentativo era maggiore o minore del numero estratto. È una ricerca di tipo binario. */
 
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 using namespace std;
 
 int main()
 {
- srand(time(NULL)); //inizializza i valori
-  //cout<<rand()%1000+1<<endl; -> COSI' STAMPEREBBE UN NUMERO COMPRESO TRA 1 E 1000
- int rd;
- rd= rand()%1000+1;
- int n;
- cout<<"guess random number"<<endl;
- cin>>n;
- 
- if (n==rd) {
-     cout<<"correct!"<<endl;
- }
- else while (n<rd) {
-     cout<<"try with a larger number"<<endl;
-     cin>>n;
- }
- while (n>rd) {
-     cout<<"try with a smaller number"<<endl;
-     cin>>n;
- }
- 
- return 0;
+    srand(time(NULL)); //inizializza i valori
+        //cout<<rand()%1000+1<<endl; -> COSI' STAMPEREBBE UN NUMERO COMPRESO TRA 1 E 1000
+    int rd;
+    rd = rand() % 1000 + 1;
+    int n;
+    cout << "guess random number" << endl;
+    cin >> n;
+
+    if (n == rd) {
+        cout << "correct!" << endl;
+    }
+    else if (n != rd) {
+        while (n < rd) {
+            cout << "try with a larger number" << endl;
+            cin >> n;
+        }
+        while (n > rd) {
+            cout << "try with a smaller number" << endl;
+            cin >> n;
+        }
+    }
 }
