e793bdf2b76b760eb4d07b5f6e378e24f4b48010
ericabertoldi <erica.bertoldi01@gmail.com>
2021-01-26 22:02:22 +0100

    es 2.1

--- esercizio_2.1/esercizio_2.1.cpp
+++ esercizio_2.1/esercizio_2.1.cpp
@@ -4,26 +4,26 @@ Info: Il limite massimo dipende da quanto grandi decidiamo che siano le struttur
 Impareremo che è possibile scrivere programmi che non hanno questo tipo di limite */
 
 #include <iostream>
-#define N 10  //inizializzazione, scritta così poi posso usarla anche in altre funzioni
+#define N 10 //inizializzazione, scritta così poi posso usarla anche in altre funzioni
 //scritta così N non è una variabile, per avere variabile dovrei scrivere int N dentro al main
 using namespace std;
 int main()
 {
-int numbers[N];
-int count=0;
+    int numbers[N];
+    int count = 0;
 
-cout<<"insert "<<N<<" numbers: "<<endl;
-for (int i=0; i<=N; i++) {
-    cin>>numbers [i];
-    if (numbers [i]==0) 
-        break;
-    count++;
-}
-int sum = numbers[0];
-for (int i=1; i<=count; i++) {
-    sum+=numbers[i];
-}
-cout<<"sum= "<<sum<<endl;
+    cout << "insert " << N << " numbers: " << endl;
+    for (int i = 0; i < N; i++) {
+        cin >> numbers[i];
+        if (numbers[i] == 0)
+            break;
+        count++;
+    }
+    int sum = numbers[0];
+    for (int i = 1; i <= count; i++) {
+        sum += numbers[i];
+    }
+    cout << "sum= " << sum << endl;
 
-return 0;
+    return 0;
 }
